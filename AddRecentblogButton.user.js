935c5e6b05e5ed25df91fd57805730eab15af5f3
NRM <82488025+CostaCiruela@users.noreply.github.com>
2023-07-14 18:02:34 -0300

    Updated script.
    Small changes that were suggested and I think they were a good idea.
    
    -Now using the default SpaceHey clock instead of an emoji.
    -The clock icon is now a clickable link. (So context menu works better now)
    -Resized the clock to match the default font size.

--- AddRecentblogButton.user.js
+++ AddRecentblogButton.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Spacehey - Recent Blogs Button
-// @version      1.0
-// @description  Add clickable clock emoji next to Blogs button, working as a "Recent blogs" link.
+// @version      2.0
+// @description  Add clickable clock next to "Blog" button, working as a "Recent blogs" link.
 // @author       NRM
 // @match        *://spacehey.com/*
 // @match        *://blog.spacehey.com/*
@@ -14,24 +14,32 @@
 (function() {
   'use strict';
 
-  // Clock Emoji Itself
-  var clockEmoji = document.createElement('span');
-  clockEmoji.innerHTML = '&#x23F0;';
-  clockEmoji.style.cursor = 'pointer';
-  clockEmoji.style.marginLeft = '5px';
+  // Clock image pointer + a/href element
+  var clockIcon = document.createElement('a');
+  clockIcon.href = 'https://blog.spacehey.com/recent';
+  clockIcon.style.cursor = 'pointer';
+  clockIcon.style.marginLeft = '5px';
+
+  // Clock image. Custom W and H
+  var clockImage = document.createElement('img');
+  clockImage.src = 'https://static.spacehey.net/icons/clock.png';
+  clockImage.style.width = '12px';
+  clockImage.style.height = '12px';
+
+  clockIcon.appendChild(clockImage);
 
   // Hover description. This is optional, delete if you don't needed.
-  clockEmoji.title = 'Recent blogs';
+  clockImage.title = 'Recent blogs';
 
   // Search blog link in the top list
-  var linksList = document.querySelector('.links');
-  var blogsLink = linksList.querySelector('a[href="https://blog.spacehey.com"]');
+  var blogsLink = document.querySelector('.links a[href="https://blog.spacehey.com"]');
 
-  // Clock emoji pos.
-  blogsLink.parentNode.insertBefore(clockEmoji, blogsLink.nextSibling);
+  // Clock image pos.
+  blogsLink.parentNode.insertBefore(clockIcon, blogsLink.nextSibling);
 
-  // Go to recent blogs when clicking the emoji!
-  clockEmoji.addEventListener('click', function() {
-    window.location.href = 'https://blog.spacehey.com/recent';
+  // Go to recent blogs when clicking the img
+  clockIcon.addEventListener('click', function(event) {
+    event.preventDefault(); // Just in case IG
+    window.location.href = clockIcon.href; // Go to href link
   });
 })();
